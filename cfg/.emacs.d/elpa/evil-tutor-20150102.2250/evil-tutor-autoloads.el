51cca84fab4fe4a06c3a79abcdf7ed1e183ed1e1
Albert Brox III <albert@exypno.tech>
2017-07-28 21:31:54 -0400

    20170728-213142

---
+++ cfg/.emacs.d/elpa/evil-tutor-20150102.2250/evil-tutor-autoloads.el
@@ -0,0 +1,29 @@
+;;; evil-tutor-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "evil-tutor" "evil-tutor.el" (22907 46736 106798
+;;;;;;  894000))
+;;; Generated autoloads from evil-tutor.el
+
+(autoload 'evil-tutor-start "evil-tutor" "\
+Start a evil-tutor session.
+
+\(fn)" t nil)
+
+(defalias 'evil-tutor-resume 'evil-tutor-start)
+
+;;;***
+
+;;;### (autoloads nil nil ("evil-tutor-pkg.el") (22907 46736 100132
+;;;;;;  227000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; evil-tutor-autoloads.el ends here
